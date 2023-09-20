512c8f9d74c6a02dba6dc941c476fad58b483abe
Ruoji Wang <ruojicris@gmail.com>
2023-09-20 09:33:13 +0800

    Finish Deque.java

--- Queues/src/Deque.java
+++ Queues/src/Deque.java
@@ -1,6 +1,9 @@
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Deque<Item> {
+public class Deque<Item> implements Iterable<Item> {
     private final Node<Item> sentinel;
     private int size;
 
@@ -54,6 +57,7 @@ public class Deque<Item> {
         size++;
         Node<Item> newTail = new Node<>();
         Node<Item> oldTail = sentinel.pre;
+        newTail.val = item;
         newTail.pre = oldTail;
         newTail.next = sentinel;
         sentinel.pre = newTail;
@@ -69,6 +73,13 @@ public class Deque<Item> {
             throw new NoSuchElementException("The deque is empty.");
         }
         size--;
+        Node<Item> oldFirst = sentinel.next;
+        Node<Item> newFirst = oldFirst.next;
+        oldFirst.pre = null;
+        oldFirst.next = null;
+        sentinel.next = newFirst;
+        newFirst.pre = sentinel;
+        return oldFirst.val;
     }
 
     /**
@@ -80,12 +91,80 @@ public class Deque<Item> {
             throw new NoSuchElementException("The deque is empty.");
         }
         size--;
+        Node<Item> oldLast = sentinel.pre;
+        Node<Item> newLast = oldLast.pre;
+        oldLast.pre = null;
+        oldLast.next = null;
+        sentinel.pre = newLast;
+        newLast.next = sentinel;
+        return oldLast.val;
     }
 
     private static class Node<Item> {
         private Item val;
         private Node<Item> pre;
         private Node<Item> next;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+        private Node<Item> cur = sentinel.next;
+
+        @Override
+        public boolean hasNext() {
+            return cur != sentinel;
+        }
+
+        @Override
+        public Item next() {
+            Item val = cur.val;
+            cur = cur.next;
+            return val;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> deque = new Deque<>();
+        for (int i = 1; i <= 5; i++) {
+            deque.addLast(i);
+            deque.addFirst(11 - i);
+        }
+
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeLast();
+
+        StdOut.println("Size: " + deque.size());
+        StdOut.print("All the elements: ");
+        for (var e : deque) {
+            StdOut.print(e + " ");
+        }
+        StdOut.println();
+
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+
+        StdOut.println("Size: " + deque.size());
+        StdOut.print("All the elements: ");
+        for (var e : deque) {
+            StdOut.print(e + " ");
+        }
+        StdOut.println();
+
     }
 
 }
