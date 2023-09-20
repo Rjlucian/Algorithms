76a58410479357f76ff647db98ddfcf15bfcc739
Ruoji Wang <ruojicris@gmail.com>
2023-09-20 11:16:22 +0800

    Finish Permutation

--- Queues/src/Permutation.java
+++ Queues/src/Permutation.java
@@ -1,2 +1,20 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+
 public class Permutation {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()) {
+            String x = StdIn.readString();
+            queue.enqueue(x);
+        }
+
+        Iterator<String> iterator = queue.iterator();
+        for (int i = 1; i <= k; i++) {
+            StdOut.println(iterator.next());
+        }
+    }
 }
